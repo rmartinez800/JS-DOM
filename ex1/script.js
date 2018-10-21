ec1bb1f36927885a48891ebbeae787292361137c
rmartinez800 <robertmartinez280@gmail.com>
2018-10-21 16:05:21 -0500

    new changes

--- ex1/script.js
+++ ex1/script.js
@@ -1,1 +1,3 @@
 // Change the text of the h1 tag to "I have changed the text"
+let titleChange = document.getElementById('title')
+titleChange.innerHTML = 'I have changed the text';
--- ex2/script.js
+++ ex2/script.js
@@ -1,1 +1,3 @@
 // Change the background color of the box to blue
+let changeBG = document.getElementById('box')
+changeBG.body.style.backgroundColor = 'blue'
